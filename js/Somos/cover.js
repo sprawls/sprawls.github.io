80e7d1c27af71d70a1107a082a8b2f65a2a35f6c
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2017-11-16 21:16:29 -0500

    somos single page first iteration

--- js/Somos/cover.js
+++ js/Somos/cover.js
@@ -1,3 +1,40 @@
-$(window).on('load', function() {
+(function() {
 
-});
+    var circle;
+    var circlePath;
+    var paths;
+    
+    function animatePath(){
+        $('.cover-section-content').toggleClass('left');
+        circlePath.animate({ d: paths.anim }, 300, mina.easein, resetPath);
+    }
+    
+    function resetPath(){
+        circlePath.animate({ d: paths.base }, 350, mina.bounce, null);
+    }
+
+    function toggleContent() {
+
+    }
+    
+    var init = function() {
+        circle = Snap('#player');
+        circlePath = circle.select('path');
+        paths = {
+            base: $('#player').attr('data-base-path'),
+            anim: $('#player').attr('data-anim-path')
+        };
+
+        $('.icon-ctn svg').on('click', function() {
+            animatePath();
+            toggleContent();
+        });
+    };
+    
+
+    $(window).ready(function() {
+        init();
+    });  
+
+})();
+
